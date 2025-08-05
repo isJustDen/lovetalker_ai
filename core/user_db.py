af8d7d1ba5e9b5b1385c8fe9520283946da7cb83
isJustDen <denis_shepelev_2013@mail.ru>
2025-08-05 14:48:26 +0500

    Add SQLite database support and user registration (/start, /profile)

---
+++ core/user_db.py
@@ -0,0 +1,52 @@
+# core/user_db.py
+
+import sqlite3
+
+# подключение к базе
+def get_connection():
+	return sqlite3.connect('data/users.db')
+
+# инициализация таблицы users
+def init_db():
+	conn = get_connection()
+	cur = conn.cursor()
+
+	#создание таблицы с информацией о поведении пользователя
+	cur.execute("""
+	CREATE TABLE IF NOT EXISTS users(
+	id INTEGER PRIMARY KEY,
+	name TEXT,
+	likes INTEGER DEFAULT 0,
+	dislikes  INTEGER DEFAULT 0
+	)
+	""")
+	conn.commit()
+	conn.close()
+
+# регистрация нового пользователя
+def register_user(user_id: int, name: str):
+	conn = get_connection()
+	cur = conn.cursor()
+
+	cur.execute("SELECT ID FROM users WHERE id = ?", (user_id,))
+	result = cur.fetchone()
+
+	if result is None:
+		cur.execute("INSERT INTO users (id, name) VALUES (?, ?)", (user_id, name))
+
+	conn.commit()
+	conn.close()
+
+# получение профиля
+def get_user(user_id: int):
+	conn = get_connection()
+	cur = conn.cursor()
+
+	cur.execute("SELECT * FROM users WHERE id = ?", (user_id,))
+	user = cur.fetchone()
+
+	conn.close()
+	return user
+
+
+
--- main.py
+++ main.py
@@ -1,16 +1,47 @@
-# This is a sample Python script.
+#main.py
+import asyncio
 
-# Press Ctrl+F5 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+from aiogram.filters import Command
+from aiogram import Bot, Dispatcher, types
+
+from config import BOT_TOKEN
+from core.user_db import register_user, get_user, init_db
+
+# создаём экземпляр бота
+
+bot = Bot(token = BOT_TOKEN)
+dp = Dispatcher()
+
+# обработчик команды /start
+@dp.message(Command("start"))
+async def start_command(message: types.Message):
+	# регистрируем пользователя в БД
+	register_user(message.from_user.id, message.from_user.first_name)
+
+	await message.answer("Привет 👋 Я Lovetalker AI 💬. Ты зарегистрирован. Готов начать знакомство!")
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press F9 to toggle the breakpoint.
+# Информация о пользователе профиля
+@dp.message(Command("profile"))
+async def profile_command(message: types.Message):
+	user = get_user(message.from_user.id)
+
+	if user:
+		user_id, name, likes, dislikes = user
+		await message.answer(
+			f"📊 Профиль: {name}\n"
+			f"👍 Лайков: {likes}\n"
+			f"👎 Дизлайков: {dislikes}"
+		)
+	else:
+		await message.answer("Ты еще не зарегистрирован, нажми /start")
 
 
-# Press the green button in the gutter to run the script.
+# функция запуска бота
+async def main():
+	init_db()
+	print("Бот запущен...")
+	await dp.start_polling(bot, skip_updates=True)
+
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+	asyncio.run(main())
